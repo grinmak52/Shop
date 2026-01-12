8e9c46da796de1b7ea2023e1b33fe15849938b0d
Skrip01 <168160103+Skrip01@users.noreply.github.com>
2026-01-12 18:44:45 +0500

    создание сущности продуктов в базе данных

--- mysite/shopapp/urls.py
+++ mysite/shopapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import shop_index
+from .views import shop_index, groups_list
 
 app_name = "shopapp"
 
 urlpatterns = [
     path("", shop_index, name="index"),
+    path("groups/", groups_list, name="groups_list"),
 ]
--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,16 +1,20 @@
 from timeit import default_timer
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.models import Group
 
 
 def shop_index(request):
     products = [
-        ('Laptop', 1999),
-        ('Desktop', 2999),
-        ('Smartphone', 999),
+        ("Laptop", 1999),
+        ("Desktop", 2999),
+        ("Smartphone", 999),
     ]
-    context = {
-        "time_running": default_timer(),
-        "products": products
-    }
+    context = {"time_running": default_timer(), "products": products}
     return render(request, "shopapp/shop-index.html", context=context)
+
+
+def groups_list(request):
+    context = {
+        "groups": Group.objects.prefetch_related("permissions").all(),
+    }
+    return render(request, "shopapp/groups-list.html", context=context)
