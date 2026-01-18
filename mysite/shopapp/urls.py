7f004c01afca7e82ea4dff0ef045de0dde2953f2
Skrip01 <168160103+Skrip01@users.noreply.github.com>
2026-01-18 20:32:12 +0500

    реализовано CBV для продуктов и заказов

--- mysite/shopapp/urls.py
+++ mysite/shopapp/urls.py
@@ -1,11 +1,32 @@
 from django.urls import path
-from .views import orders_list, shop_index, groups_list, products_list
+from .views import (
+    shop_index,
+    groups_list,
+    ProductsListView,
+    ProductCreateView,
+    ProductDetailView,
+    ProductUpdateView,
+    ProductDeleteView,
+    OrdersListView,
+    OrderCreateView,
+    OrderDetailView,
+    OrderUpdateView,
+    OrderDeleteView,
+)
 
 app_name = "shopapp"
 
 urlpatterns = [
     path("", shop_index, name="index"),
     path("groups/", groups_list, name="groups_list"),
-    path("products/", products_list, name="products_list"),
-    path("orders/", orders_list, name="orders_list"),
+    path("products/", ProductsListView.as_view(), name="products_list"),
+    path("products/create", ProductCreateView.as_view(), name="create_product"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("products/<int:pk>/update", ProductUpdateView.as_view(), name="product_update"),
+    path("products/<int:pk>/delete", ProductDeleteView.as_view(), name="product_dlete"),
+    path("orders/", OrdersListView.as_view(), name="orders_list"),
+    path("orders/create", OrderCreateView.as_view(), name="create_order"),
+    path("orders/<int:pk>/", OrderDetailView.as_view(), name="order_detail"),
+    path("orders/<int:pk>/update", OrderUpdateView.as_view(), name="order_update"),
+    path("orders/<int:pk>/delete", OrderDeleteView.as_view(), name="order_delete"),
 ]
--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,8 +1,18 @@
 from timeit import default_timer
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
+from django.urls import reverse_lazy
 
 from .models import Product, Order
+from .forms import OrderForm
 
 
 def shop_index(request):
@@ -21,14 +31,87 @@ def groups_list(request):
     }
     return render(request, "shopapp/groups-list.html", context=context)
 
-def products_list(request):
-    context = {
-        "products": Product.objects.all(),
-    }
-    return render(request, "shopapp/products-list.html", context=context)
 
-def orders_list(request):
-    context = {
-        "orders": Order.objects.select_related('user').prefetch_related('products').all(),
-    }
-    return render(request, "shopapp/orders-list.html", context=context)
+class ProductsListView(ListView):
+    model = Product
+    template_name = "shopapp/products-list.html"
+    context_object_name = "products"
+
+
+class ProductDetailView(DetailView):
+    template_name = "shopapp/product-detail.html"
+    model = Product
+    context_object_name = "product"
+
+
+class ProductCreateView(CreateView):
+    template_name = "shopapp/product-create.html"
+    model = Product
+    fields = "name", "price", "description", "discount"
+    success_url = reverse_lazy("shopapp:products_list")
+
+
+class ProductUpdateView(UpdateView):
+    template_name = "shopapp/product-update.html"
+    fields = "name", "price", "description", "discount"
+    model = Product
+
+    def get_success_url(self):
+        return reverse(
+            "shopapp:product_detail",
+            kwargs={"pk": self.object.pk},
+        )
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy("shopapp:products_list")
+
+
+class OrdersListView(ListView):
+    queryset = Order.objects.select_related("user").prefetch_related("products")
+    template_name = "shopapp/orders-list.html"
+    context_object_name = "orders"
+
+
+class OrderDetailView(DetailView):
+    template_name = "shopapp/order-detail.html"
+    queryset = Order.objects.select_related("user").prefetch_related("products")
+    context_object_name = "order"
+
+
+class OrderCreateView(CreateView):
+    template_name = "shopapp/order-create.html"
+    queryset = Order.objects.select_related("user").prefetch_related("products")
+    fields = "delivery_address", "promocode", "products"
+    success_url = reverse_lazy("shopapp:orders_list")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class OrderUpdateView(UpdateView):
+    model = Order
+    template_name = "shopapp/order-update.html"
+    fields = ("delivery_address", "promocode", "products")
+
+    def get_queryset(self):
+        return (
+            Order.objects
+            .select_related("user")
+            .prefetch_related("products")
+            .filter(user=self.request.user)
+        )
+
+    def get_success_url(self):
+        return reverse(
+            "shopapp:order_detail",
+            kwargs={"pk": self.object.pk},
+        )
+        
+
+class OrderDeleteView(DeleteView):
+    template_name = "shopapp/order_confirm_delete.html"
+    queryset = Order.objects.select_related("user").prefetch_related("products")
+    success_url = reverse_lazy("shopapp:orders_list")
